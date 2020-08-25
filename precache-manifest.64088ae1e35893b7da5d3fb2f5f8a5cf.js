483ffe924b3397496a13f7ced06069f18bd284ac
Sarita <dsarritta@gmail.com>
2020-08-25 06:44:26 +0545

    Updates

---
+++ precache-manifest.64088ae1e35893b7da5d3fb2f5f8a5cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838eb17b735b44e0e6e595064f1611e8",
+    "url": "/Plant-World/index.html"
+  },
+  {
+    "revision": "ad8096b05e4456927508",
+    "url": "/Plant-World/static/css/main.8f773246.chunk.css"
+  },
+  {
+    "revision": "88ad88f01b004780c1df",
+    "url": "/Plant-World/static/js/2.308d8a0f.chunk.js"
+  },
+  {
+    "revision": "aa4039141ac8bca354206cb14ee23eb2",
+    "url": "/Plant-World/static/js/2.308d8a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8096b05e4456927508",
+    "url": "/Plant-World/static/js/main.fa14a5e0.chunk.js"
+  },
+  {
+    "revision": "5e06732bdad0018dcd57",
+    "url": "/Plant-World/static/js/runtime-main.70045cb0.js"
+  },
+  {
+    "revision": "aaf3b7818816f9301bcf97c5e28f2108",
+    "url": "/Plant-World/static/media/background.aaf3b781.jpg"
+  }
+]);
